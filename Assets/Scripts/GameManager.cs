0fa343712129efb87535ad761d13d9031323abc9
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-08 04:53:08 +0300

    🔨 GameManager singleton implementation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    
+    public static GameManager Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
 }
