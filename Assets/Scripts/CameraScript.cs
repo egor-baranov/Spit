fb9c5fae54ae72d60fe6aff0fc405d27411593de
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-08 04:52:45 +0300

    ✨ CameraScript prototype

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraScript: MonoBehaviour {
+    public static CameraScript Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
+}
