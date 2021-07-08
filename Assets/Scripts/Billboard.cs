d36d224e65d2beb32dc8e134fccf51ceac131c5f
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-08 23:20:44 +0300

    🔨 Billboard class

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+    [SerializeField] private bool shouldRotate;
+
+    private void LateUpdate() {
+        if (shouldRotate) {
+            transform.rotation = Player.Instance.Body.transform.rotation;
+        }
+    }
+}
