d77d421a8caef5f6292ea7f43e0fd0c8913ce132
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-08 05:06:12 +0300

    🔨 CameraScript._distanceFromPlayer, .movementSpeed, .rotationSpeed, going to the player

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-public class CameraScript: MonoBehaviour {
+public class CameraScript : MonoBehaviour {
     public static CameraScript Instance { get; private set; }
+    private Vector3 _distanceFromPlayer;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
 
     private void Awake() {
         if (Instance != null) {
@@ -9,5 +13,17 @@ public class CameraScript: MonoBehaviour {
         }
 
         Instance = this;
+    }
+
+    private void Start() {
+        _distanceFromPlayer = transform.position - Player.Instance.transform.position;
+    }
+
+    public void Update() {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            Player.Instance.transform.position + _distanceFromPlayer,
+            Time.deltaTime * movementSpeed
+        );
     }
 }
