fc2058231356bc9558d162591ef5bbcf3a4aab0e
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-10 20:34:00 +0300

    ✨ Spit prefab and script

---
+++ Assets/Scripts/Spit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class Spit : MonoBehaviour {
+    private void Awake() {
+        Destroy(gameObject, 5);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.parent == null) {
+            Destroy(gameObject);
+        }
+
+        if (other.transform.parent.GetComponent<Enemy>()) {
+            Player.Instance.MoveTo(other.transform.parent.GetComponent<Enemy>());
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy() {
+        Player.Instance.Recharge();
+    }
+}
