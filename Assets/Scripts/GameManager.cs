17027fdae89896d94cd93dcfb1cf72f08df0c5e4
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-10 20:34:56 +0300

    ✨ Game Manager auto enemy instantiate

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     public static GameManager Instance { get; private set; }
 
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private int enemyCount;
+
     private void Awake() {
         if (Instance != null) {
             Destroy(gameObject);
         }
 
         Instance = this;
+
+        for (var i = 0; i < enemyCount; i++) {
+            Instantiate(enemyPrefab);
+        }
     }
 }
