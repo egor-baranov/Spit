f3ca845076d8e8b11bcaae9f6fbaade8a895393f
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-12 02:54:51 +0300

    🔨 GameManager.OnDeath method

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using Core.Managers;
+using UnityEngine;
+
+namespace Core {
+    public class GameManager : MonoBehaviour {
+        public static GameManager Instance { get; private set; }
+
+        [SerializeField] private GameObject enemyPrefab;
+        [SerializeField] private int enemyCount;
+
+        public void OnDeath() => UiManager.Instance.OnDeath();
+
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+            }
+
+            Instance = this;
+
+            for (var i = 0; i < enemyCount; i++) {
+                Instantiate(enemyPrefab);
+            }
+        }
+    }
+}
