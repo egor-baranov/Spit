2b6e6e97e81b125540baf81e75fa8393fec86119
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-12 04:38:01 +0300

    ✨ Bullet class and prefab

---
+++ Assets/Scripts/Controllers/Projectiles/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Controllers.Creatures;
+using UnityEngine;
+
+namespace Controllers.Projectiles {
+    public class Bullet : Projectile {
+        [SerializeField] private float damage;
+
+        private void OnTriggerEnter(Collider other) {
+            try {
+                var enemy = other.transform.parent.GetComponent<Enemy>();
+                if (enemy.gameObject != Player.Instance.gameObject) {
+                    enemy.ReceiveDamage(damage);
+                    Destroy(gameObject);
+                }
+            }
+            catch (NullReferenceException) { }
+        }
+
+        protected override void OnDestroy() {
+            base.OnDestroy();
+        }
+    }
+}
