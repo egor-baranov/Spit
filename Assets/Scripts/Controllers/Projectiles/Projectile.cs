bb93a535d923fbfc95027533ba33eed5c8d6c127
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-12 04:37:02 +0300

    ✨ Projectile base class for Bullet and Spit

---
+++ Assets/Scripts/Controllers/Projectiles/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Controllers.Projectiles {
+    public class Projectile : MonoBehaviour {
+        public float MovementSpeed => movementSpeed;
+
+        [SerializeField] protected float maxTimeAlive;
+        [SerializeField] protected float movementSpeed;
+
+        protected virtual void Awake() { }
+        protected virtual void Start() { }
+        protected virtual void Update() { }
+
+        protected virtual void OnDestroy() { }
+    }
+}
