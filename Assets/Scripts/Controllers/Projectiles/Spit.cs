75f7ae6bc4b88816641a24c8982493e3e8dc9781
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-12 04:37:37 +0300

    🔨 Made Spit child class of Projectile

---
+++ Assets/Scripts/Controllers/Projectiles/Spit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Controllers.Creatures;
+using UnityEngine;
+
+namespace Controllers.Projectiles {
+    public class Spit : Projectile {
+        private float _timeAliveLeft;
+        
+        protected override void Awake() {
+            base.Awake();
+            _timeAliveLeft = maxTimeAlive;
+            Destroy(gameObject, maxTimeAlive);
+        }
+
+        protected override void Update() {
+            base.Awake();
+            _timeAliveLeft -= Time.deltaTime;
+            GetComponent<Light>().intensity = 30 * _timeAliveLeft / maxTimeAlive;
+            GetComponent<Light>().range = 30 * _timeAliveLeft / maxTimeAlive;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            try {
+                if (other.transform.parent.GetComponent<Enemy>()) {
+                    Player.Instance.MoveTo(other.transform.parent.GetComponent<Enemy>());
+                    Destroy(gameObject);
+                }
+            }
+            catch (NullReferenceException) {
+                GetComponent<Rigidbody>().velocity = -GetComponent<Rigidbody>().velocity * 0.75F;
+            }
+        }
+
+        protected override void OnDestroy() {
+            base.OnDestroy();
+            Player.Instance.RechargeSoulBlast();
+        }
+    }
+}
