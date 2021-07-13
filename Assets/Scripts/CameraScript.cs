a9618184a0c3513d25bed384fcd4b113cc4e7520
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-13 04:30:57 +0300

    🔨 Smooth camera movement and rotation (working with little bugs)

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
     public static CameraScript Instance { get; private set; }
+    private Transform CameraHolder => Player.Instance.CameraHolder.transform;
+
     private Vector3 _distanceFromPlayer;
     [SerializeField] private float movementSpeed;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float cameraDistance;
+    [SerializeField] private float zoomOutScale;
 
-    private float _targetCameraZ = -100F;
+    private float _targetCameraZ;
 
     private void Awake() {
         if (Instance != null) {
@@ -15,27 +19,39 @@ public class CameraScript : MonoBehaviour {
         }
 
         Instance = this;
+        _targetCameraZ = -cameraDistance;
     }
 
     private void Start() {
-        _distanceFromPlayer = transform.position - Player.Instance.transform.position;
+        _distanceFromPlayer = (transform.position - Player.Instance.transform.position).normalized * cameraDistance;
+        transform.LookAt(Player.Instance.transform);
     }
 
-    public void Update() {
-        transform.LookAt(Player.Instance.transform);
-
-        transform.localPosition = Vector3.Lerp(
-            transform.localPosition,
-            new Vector3(0, 0, _targetCameraZ),
-            30 * Time.deltaTime
-        );
+    public void LateUpdate() {
+        if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E)) {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                CameraHolder.position - _distanceFromPlayer,
+                Time.deltaTime * movementSpeed
+            );
+        }
 
         if (Input.GetKeyDown(KeyCode.Mouse1)) {
-            _targetCameraZ = -200F;
+            _distanceFromPlayer *= zoomOutScale;
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse1)) {
-            _targetCameraZ = -100F;
+            _distanceFromPlayer /= zoomOutScale;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.E)) {
+            _distanceFromPlayer = (transform.position - Player.Instance.transform.position).normalized *
+                                  (cameraDistance * (Input.GetKey(KeyCode.Mouse1) ? zoomOutScale : 1));
+            transform.parent = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E)) {
+            transform.parent = CameraHolder;
         }
     }
 }
