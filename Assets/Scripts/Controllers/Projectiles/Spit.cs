76545dad01e34c6e82a37ba32fbd2c9588746370
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-16 05:56:39 +0300

    ✨ Spit time slowing when enemy is nearby

--- Assets/Scripts/Controllers/Projectiles/Spit.cs
+++ Assets/Scripts/Controllers/Projectiles/Spit.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Linq;
 using Controllers.Creatures;
+using Core;
 using UnityEngine;
 
 namespace Controllers.Projectiles {
     public class Spit : Projectile {
+        [SerializeField] private float slowTimeDistance;
+        [SerializeField] private float slowTimeScale;
+        [SerializeField] private float slowmoTimeout;
+
         private float _timeAliveLeft;
-        
+
         protected override void Awake() {
             base.Awake();
             _timeAliveLeft = maxTimeAlive;
@@ -17,23 +23,46 @@ namespace Controllers.Projectiles {
             _timeAliveLeft -= Time.deltaTime;
             GetComponent<Light>().intensity = 30 * _timeAliveLeft / maxTimeAlive;
             GetComponent<Light>().range = 30 * _timeAliveLeft / maxTimeAlive;
+
+            Time.timeScale = GameManager.Instance.EnemyList.Any(
+                it =>
+                    Vector3.Distance(
+                        transform.position,
+                        it.transform.position
+                    ) <= slowTimeDistance
+            )
+                ? slowTimeScale
+                : 1F;
+            Debug.Log(GameManager.Instance.EnemyList.Min(
+                it =>
+                    Vector3.Distance(
+                        transform.position,
+                        it.transform.position
+                    )
+            ));
         }
 
         private void OnTriggerEnter(Collider other) {
             try {
-                if (other.transform.parent.GetComponent<Enemy>()) {
-                    Player.Instance.MoveTo(other.transform.parent.GetComponent<Enemy>());
-                    Destroy(gameObject);
+                if (!other.transform.parent.GetComponent<Enemy>()) {
+                    return;
                 }
+
+                Player.Instance.MoveTo(other.transform.parent.GetComponent<Enemy>());
+                GlobalScope.ExecuteWithDelay(
+                    slowmoTimeout,
+                    () => Time.timeScale = 1F,
+                    () => Time.timeScale = slowTimeScale
+                );
+                Destroy(gameObject);
             }
-            catch (NullReferenceException) {
-                GetComponent<Rigidbody>().velocity = -GetComponent<Rigidbody>().velocity * 0.75F;
-            }
+            catch (NullReferenceException) { }
         }
 
         protected override void OnDestroy() {
             base.OnDestroy();
             Player.Instance.RechargeSoulBlast();
+            Time.timeScale = 1F;
         }
     }
 }
