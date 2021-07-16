49bea0e6758da594ceac30c08b4470c18d866deb
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-16 05:53:08 +0300

    🔨 Fitted camera position into ranges

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,51 +1,37 @@
 ﻿using Controllers.Creatures;
 using UnityEngine;
+using Util.Data;
 
 public class CameraScript : MonoBehaviour {
     public static CameraScript Instance { get; private set; }
-    private Transform CameraHolder => Player.Instance.CameraHolder.transform;
+    private static Transform CameraHolder => Player.Instance.CameraHolder.transform;
 
-    private Vector3 _distanceFromPlayer;
+    private Vector3 _distanceFromTarget;
     [SerializeField] private float movementSpeed;
-    [SerializeField] private float rotationSpeed;
     [SerializeField] private float cameraDistance;
     [SerializeField] private float zoomOutScale;
 
-    private float _targetCameraZ;
-
-    private void Awake() {
-        if (Instance != null) {
-            Destroy(gameObject);
-        }
-
-        Instance = this;
-        _targetCameraZ = -cameraDistance;
-    }
-
-    private void Start() {
-        _distanceFromPlayer = (transform.position - Player.Instance.transform.position).normalized * cameraDistance;
-        transform.LookAt(Player.Instance.transform);
-    }
-
+    [SerializeField] private MinMaxFloat cameraRangeX, cameraRangeZ;
+    
     public void LateUpdate() {
         if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E)) {
             transform.position = Vector3.Lerp(
                 transform.position,
-                CameraHolder.position - _distanceFromPlayer,
+                Fit(CameraHolder.position - _distanceFromTarget),
                 Time.deltaTime * movementSpeed
             );
         }
 
         if (Input.GetKeyDown(KeyCode.Mouse1)) {
-            _distanceFromPlayer *= zoomOutScale;
+            _distanceFromTarget *= zoomOutScale;
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse1)) {
-            _distanceFromPlayer /= zoomOutScale;
+            _distanceFromTarget /= zoomOutScale;
         }
 
         if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.E)) {
-            _distanceFromPlayer = (transform.position - Player.Instance.transform.position).normalized *
+            _distanceFromTarget = (transform.position - CameraHolder.position).normalized *
                                   (cameraDistance * (Input.GetKey(KeyCode.Mouse1) ? zoomOutScale : 1));
             transform.parent = null;
         }
@@ -53,5 +39,21 @@ public class CameraScript : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E)) {
             transform.parent = CameraHolder;
         }
+    }
+
+    private Vector3 Fit(Vector3 position) =>
+        new Vector3(cameraRangeX.Fit(position.x), position.y, cameraRangeZ.Fit(position.z));
+    
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
+
+    private void Start() {
+        _distanceFromTarget = (transform.position - CameraHolder.position).normalized * cameraDistance;
+        transform.LookAt(CameraHolder);
     }
 }
