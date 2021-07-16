48fbdfa724a4a94559360d84e3f9cb00c7576d1f
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-16 05:54:14 +0300

    ✨ MinMaxFloat class

--- Assets/Scripts/Util/Data/MinMax.cs
+++ Assets/Scripts/Util/Data/MinMax.cs
@@ -18,4 +18,22 @@ namespace Util.Data {
 
         public bool Contains(int value) => min <= value && max >= value;
     }
+
+    [Serializable]
+    public struct MinMaxFloat {
+        public float Min => min;
+
+        public float Max => max;
+
+        [SerializeField] private float min;
+        [SerializeField] private float max;
+
+        public MinMaxFloat(float min = -1, float max = -1) {
+            this.min = min;
+            this.max = max;
+        }
+
+        public bool Contains(float value) => min <= value && max >= value;
+        public float Fit(float value) => value > max ? max : value < min ? min : value;
+    }
 }
