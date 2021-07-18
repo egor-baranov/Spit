bf59283678d1624b3595e8d6e463eb766a8b77ad
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-18 22:26:28 +0300

    🔨 Creature.BodyIntent class, .AcceptIntent and .SwapWith methods

--- Assets/Scripts/Controllers/Creatures/Creature.cs
+++ Assets/Scripts/Controllers/Creatures/Creature.cs
@@ -21,14 +21,33 @@ namespace Controllers.Creatures {
         }
 
         public GameObject Body => transform.Find("Body").gameObject;
-        protected SliderObject HpSlider => Body.transform.Find("HP Slider").GetComponent<SliderObject>();
+        protected bool IsAlive => HealthPoints > 0;
+        protected BodyIntent Intent => BodyIntent.Create(this);
+        private SliderObject HpSlider => Body.transform.Find("HP Slider").GetComponent<SliderObject>();
 
         private float _healthPoints;
         [SerializeField] protected float maxHp;
         [SerializeField] protected float movementSpeed;
 
-        public bool IsAlive => HealthPoints > 0;
         public void ReceiveDamage(float damage) => HealthPoints -= damage;
+
+        public void SwapWith(Creature other) {
+            AcceptIntent(other.AcceptIntent(Intent));
+            OnSwap();
+            other.OnSwap();
+        }
+
+        protected BodyIntent AcceptIntent(BodyIntent intent) {
+            var prevIntent = Intent;
+
+            _healthPoints = intent.HealthPoints;
+            maxHp = intent.MaxHealthPoints;
+            movementSpeed = intent.MovementSpeed;
+            Body.GetComponent<SpriteRenderer>().sprite = intent.Sprite;
+            transform.position = intent.Position;
+
+            return prevIntent;
+        }
 
         protected virtual void Awake() => HealthPoints = maxHp;
 
@@ -42,6 +61,34 @@ namespace Controllers.Creatures {
             }
             else {
                 Destroy(gameObject);
+            }
+        }
+
+        protected virtual void OnSwap() { }
+
+
+        protected class BodyIntent {
+            public float HealthPoints { get; }
+            public float MaxHealthPoints { get; }
+            public float MovementSpeed { get; }
+            public Sprite Sprite { get; }
+            public Vector3 Position { get; }
+
+            public static BodyIntent Create(Creature creature) =>
+                new BodyIntent(
+                    creature.HealthPoints,
+                    creature.MaxHp,
+                    creature.movementSpeed,
+                    creature.Body.GetComponent<SpriteRenderer>().sprite,
+                    creature.transform.position
+                );
+
+            private BodyIntent(float hp, float maxHp, float movementSpeed, Sprite sprite, Vector3 position) {
+                HealthPoints = hp;
+                MaxHealthPoints = maxHp;
+                MovementSpeed = movementSpeed;
+                Sprite = sprite;
+                Position = position;
             }
         }
     }
