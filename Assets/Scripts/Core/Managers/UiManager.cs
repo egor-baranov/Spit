408d32806c38f45d51b969f9685ce968e6bf7406
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-19 04:21:10 +0300

    🎨 Score displaying in UiManager

--- Assets/Scripts/Core/Managers/UiManager.cs
+++ Assets/Scripts/Core/Managers/UiManager.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Core.Managers {
     public class UiManager : MonoBehaviour {
         public static UiManager Instance { get; private set; }
 
+        private Text ScoreText => deathPanel.transform.Find("Score").GetComponent<Text>();
+
         [SerializeField] private GameObject deathPanel;
 
-        public void OnDeath() => deathPanel.SetActive(true);
+        public void OnDeath(int score) {
+            deathPanel.SetActive(true);
+            ScoreText.text = $"Score: {score}";
+        }
+
         public void OnRestart() => GameManager.OnRestart();
 
         private void Awake() {
