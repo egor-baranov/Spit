bf15ded418c1d19a9da08be35e23ae8f3faa6d5f
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-19 06:34:48 +0300

    🐛 Fixed bullets destroying after hit sender class

--- Assets/Scripts/Controllers/Projectiles/Bullet.cs
+++ Assets/Scripts/Controllers/Projectiles/Bullet.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 namespace Controllers.Projectiles {
     public class Bullet : Projectile {
         [SerializeField] private float damage;
+
         private BulletTarget _target;
         public void SetColor(Color color) => GetComponent<Light>().color = color;
         public void SetTarget(BulletTarget target) => _target = target;
 
         private void OnTriggerEnter(Collider other) {
+            if (other.GetComponent<Bullet>()) {
+                return;
+            }
+
             var enemy = other.GetComponent<Enemy>();
             var player = other.GetComponent<Player>();
+
+            if (_target == BulletTarget.Enemy && player != null || _target == BulletTarget.Player && enemy != null) {
+                return;
+            }
 
             if ((_target == BulletTarget.Enemy || _target == BulletTarget.Everything) && enemy != null) {
                 enemy.ReceiveDamage(damage);
