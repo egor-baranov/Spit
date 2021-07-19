7cf6de7a22426abdd12a7c9fca10e56c4d7f6bb6
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-19 04:13:13 +0300

    🔨 Bullet.SetColor and .SetTarget methods, BulletTarget class and ability to receive damage to player

--- Assets/Scripts/Controllers/Projectiles/Bullet.cs
+++ Assets/Scripts/Controllers/Projectiles/Bullet.cs
@@ -1,24 +1,32 @@
-﻿using System;
-using Controllers.Creatures;
+﻿using Controllers.Creatures;
 using UnityEngine;
 
 namespace Controllers.Projectiles {
     public class Bullet : Projectile {
         [SerializeField] private float damage;
+        private BulletTarget _target;
+        public void SetColor(Color color) => GetComponent<Light>().color = color;
+        public void SetTarget(BulletTarget target) => _target = target;
 
         private void OnTriggerEnter(Collider other) {
-            try {
-                var enemy = other.transform.parent.GetComponent<Enemy>();
-                if (enemy.gameObject != Player.Instance.gameObject) {
-                    enemy.ReceiveDamage(damage);
-                    Destroy(gameObject);
-                }
+            var enemy = other.GetComponent<Enemy>();
+            var player = other.GetComponent<Player>();
+
+            if ((_target == BulletTarget.Enemy || _target == BulletTarget.Everything) && enemy != null) {
+                enemy.ReceiveDamage(damage);
             }
-            catch (NullReferenceException) { }
+
+            if ((_target == BulletTarget.Player || _target == BulletTarget.Everything) && player != null) {
+                player.ReceiveDamage(damage);
+            }
+
+            Destroy(gameObject);
         }
 
-        protected override void OnDestroy() {
-            base.OnDestroy();
+        public enum BulletTarget {
+            Enemy,
+            Player,
+            Everything
         }
     }
 }
