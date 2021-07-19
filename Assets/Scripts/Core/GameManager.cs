14088285f18da1e1c6ab4c48210fe1643f647c16
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-19 04:19:01 +0300

    🔨 Score calculating, .SpawnEnemyAt method

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Controllers.Creatures;
 using Core.Managers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Util.ExtensionMethods;
+using Random = UnityEngine.Random;
 
 namespace Core {
     public class GameManager : MonoBehaviour {
         public static GameManager Instance { get; private set; }
+
+        public static int soulShotCount = 0, killedEnemiesCount = 0;
 
         public IEnumerable<Enemy> EnemyList => _enemyList;
 
@@ -17,10 +19,24 @@ namespace Core {
 
         private readonly List<Enemy> _enemyList = new List<Enemy>();
 
-        public static void OnDeath() => UiManager.Instance.OnDeath();
+        public static void OnDeath() =>
+            UiManager.Instance.OnDeath(
+                (int) Time.timeSinceLevelLoad * (soulShotCount + 1) * (killedEnemiesCount + 1)
+            );
+
         public static void OnRestart() => SceneManager.LoadScene("Prototype");
-        
-        public void SpawnEnemies(int count) => 0.Until(count).ToList().ForEach(it => Instantiate(enemyPrefab));
+
+        public Enemy SpawnEnemyAt(Vector3 position) =>
+            Instantiate(enemyPrefab, position, Quaternion.identity).GetComponent<Enemy>();
+
+        public void SpawnEnemies(int count) => 0.Until(count).ToList().ForEach(it =>
+            Instantiate(
+                enemyPrefab,
+                new Vector3(Random.Range(-94, 94), 6, Random.Range(-142, 150)),
+                Quaternion.identity
+            )
+        );
+
         public void RegisterEnemy(Enemy enemy) => _enemyList.Add(enemy);
         public void RemoveEnemy(Enemy enemy) => _enemyList.Remove(enemy);
 
