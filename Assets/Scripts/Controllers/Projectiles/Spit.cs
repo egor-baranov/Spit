24cbd78289c3932dd4f414977230ba41999de521
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-19 04:20:27 +0300

    🔨 Advanced spit functionality

--- Assets/Scripts/Controllers/Projectiles/Spit.cs
+++ Assets/Scripts/Controllers/Projectiles/Spit.cs
@@ -10,32 +10,39 @@ namespace Controllers.Projectiles {
         [SerializeField] private float slowmoTimeout;
 
         private float _timeAliveLeft;
+        private bool _killPlayer = true;
 
         protected override void Awake() {
             base.Awake();
             _timeAliveLeft = maxTimeAlive;
             Destroy(gameObject, maxTimeAlive);
+
+            CameraScript.Instance.SetTarget(transform);
         }
 
         protected override void Update() {
             base.Awake();
             _timeAliveLeft -= Time.deltaTime;
-            GetComponent<Light>().intensity = 30 * _timeAliveLeft / maxTimeAlive;
-            GetComponent<Light>().range = 30 * _timeAliveLeft / maxTimeAlive;
+            GetComponent<Light>().intensity = 30 * Mathf.Sqrt(_timeAliveLeft / maxTimeAlive);
+            GetComponent<Light>().range = 30 * Mathf.Sqrt(_timeAliveLeft / maxTimeAlive);
         }
 
         private void OnCollisionEnter(Collision other) {
             try {
-                if (!other.collider.GetComponent<Enemy>()) {
+                if (!other.collider.GetComponent<Enemy>() || _timeAliveLeft > maxTimeAlive - 0.02F) {
                     return;
                 }
 
+                _killPlayer = false;
+                GameManager.soulShotCount += 1;
                 Player.Instance.SwapWith(other.collider.GetComponent<Enemy>());
                 GlobalScope.ExecuteWithDelay(
                     slowmoTimeout,
                     () => Time.timeScale = 1F,
                     () => Time.timeScale = slowTimeScale
                 );
+                GameManager.Instance.RemoveEnemy(other.gameObject.GetComponent<Enemy>());
+                Destroy(other.gameObject);
                 Destroy(gameObject);
             }
             catch (NullReferenceException) { }
@@ -43,7 +50,15 @@ namespace Controllers.Projectiles {
 
         protected override void OnDestroy() {
             base.OnDestroy();
+
+            if (_killPlayer) {
+                Player.Instance.HealthPoints = 0;
+                return;
+            }
+
             Player.Instance.RechargeSoulBlast();
+            CameraScript.Instance.SetTarget(Player.Instance.CameraHolder.transform);
+            Player.Instance.UnFreeze();
         }
     }
 }
