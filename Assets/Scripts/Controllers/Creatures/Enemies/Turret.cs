18d6e637b6fbb47004d97c7d3cf5ef8c80459282
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-07-30 06:34:08 +0300

    🚧 Turret class with shooting (in progress)

---
+++ Assets/Scripts/Controllers/Creatures/Enemies/Turret.cs
@@ -0,0 +1,37 @@
+﻿using Controllers.Creatures.Base;
+using Controllers.Creatures.Enemies.Base;
+using Controllers.Projectiles;
+using Core;
+using UnityEngine;
+
+namespace Controllers.Creatures.Enemies {
+    public class Turret : Enemy {
+        private Transform Center => transform.Find("Center");
+
+        protected override Vector3 ShootPosition => transform.position + _shootDirection.normalized * 18;
+
+        protected override void Shoot() {
+            if (!CanShoot) return;
+            base.Shoot();
+
+            var bullet = Instantiate(bulletPrefab, ShootPosition, Quaternion.identity)
+                .GetComponent<Bullet>()
+                .SetTarget(Bullet.BulletTarget.Everything)
+                .SetColor(Color.green)
+                .SetDamageModifier(0.5F)
+                .SetSpeedModifier(1.2F)
+                .SetModifiers(enemyModifier: 0.5F)
+                .SetScale(0.6F);
+
+            bullet.gameObject.GetComponent<Rigidbody>().velocity =
+                Quaternion.AngleAxis(Random.Range(-10F, 10F), Vector3.up) * _shootDirection.normalized *
+                bullet.GetComponent<Projectile>().MovementSpeed;
+        }
+
+        protected override void Update() {
+            base.Update();
+            Center.transform.LookAt(Target);
+            ApplyAutoShooting();
+        }
+    }
+}
