a5b4e3ee8de273e252d2fb268285c1443955af45
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-08-06 05:44:44 +0300

    ⚡ Advanced Assassin AI, runaway distance parameter, fixed shooting through walls

--- Assets/Scripts/Controllers/Creatures/Enemies/Assassin.cs
+++ Assets/Scripts/Controllers/Creatures/Enemies/Assassin.cs
@@ -1,14 +1,20 @@
-﻿using Controllers.Creatures.Base;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Controllers.Creatures.Base;
 using Controllers.Creatures.Enemies.Base;
 using Controllers.Projectiles;
 using Controllers.Projectiles.Base;
 using Core;
 using Pathfinding;
 using UnityEngine;
+using Util.ExtensionMethods;
 
 namespace Controllers.Creatures.Enemies {
     public class Assassin : Enemy {
         public override EnemyType Type => EnemyType.Assassin;
+
+        [SerializeField] private float runAwayDistance;
+        private Vector3 _lastPoint;
 
         public override Bullet.Builder BulletBuilder(Vector3 bulletPosition) =>
             new Bullet.Builder(
@@ -28,6 +34,7 @@ namespace Controllers.Creatures.Enemies {
                 .SetTarget(Bullet.BulletTarget.Everything)
                 .SetColor(Color.green)
                 .SetModifiers(enemyModifier: 0.5F)
+                .SetSpeedModifier(0.5F)
                 .Result();
 
             bullet.gameObject.GetComponent<Rigidbody>().velocity =
@@ -49,7 +56,11 @@ namespace Controllers.Creatures.Enemies {
                     AstarPath.active.UpdateGraphs(prevGuo);
 
                     prevGuo = guo;
-                }, () => false);
+                },
+                () => false
+            );
+
+            PickNextPoint();
         }
 
         protected override void Update() {
@@ -57,29 +68,63 @@ namespace Controllers.Creatures.Enemies {
                 Target = Player.Instance.transform;
             }
 
+            ShootDirection = Target.position - transform.position;
+            ShootDirection = new Vector3(ShootDirection.x, 0, ShootDirection.z);
+
             if (Vector3.Distance(transform.position, Target.position) > maxDistanceFromPlayer) {
-                Freeze();
                 return;
             }
 
-            if (!Physics.SphereCast(ShootPosition, 10, Target.position, out var hit) ||
-                !hit.collider.gameObject.GetComponent<Player>()) {
-                UnFreeze();
-            }
 
-            if (Vector3.Distance(transform.position, Target.position) <= maxShootDistance) {
-                ShootDirection = Target.position - transform.position;
-                ShootDirection = new Vector3(ShootDirection.x, 0, ShootDirection.z);
-                Shoot();
-                Freeze();
+            if (Vector3.Distance(_lastPoint, Target.position) > maxShootDistance ||
+                Vector3.Distance(_lastPoint, Target.position) < runAwayDistance || Physics.Linecast(
+                    _lastPoint,
+                    Target.transform.position,
+                    GameManager.Instance.WallLayerMask)) {
+                PickNextPoint();
             }
             else {
-                UnFreeze();
+                Shoot();
             }
         }
 
         protected override void OnSwap(Creature other) {
             GlobalScope.ExecuteWithDelay(3, UnFreeze, Freeze);
+        }
+
+        private void PickNextPoint() {
+            var positionList = 0.Until(300)
+                .Select(_ => Random.insideUnitCircle * maxShootDistance)
+                .Select(circle => Target.transform.position + new Vector3(circle.x, 0, circle.y))
+                .Where(point =>
+                    !Physics.OverlapSphere(point, 20, GameManager.Instance.WallLayerMask).Any() &&
+                    !Physics.Linecast(point, Target.transform.position, GameManager.Instance.WallLayerMask)
+                )
+                .ToList();
+
+            if (positionList.IsEmpty()) {
+                return;
+            }
+
+            _lastPoint = positionList
+                .OrderBy(it => -Vector3.Distance(it, Target.transform.position))
+                .First();
+
+            GetComponent<Seeker>().StartPath(
+                transform.position,
+                _lastPoint,
+                OnPathComplete
+            );
+        }
+
+        private void OnPathComplete(Path p) {
+            //We got our path back
+            if (p.error) {
+                // a valid path couldn't be found
+            }
+            else {
+                // now we can get a Vector3 representation of the path from p.vectorPath
+            }
         }
     }
 }
