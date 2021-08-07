bf6f8f95a7fa47c3bf0b8ce74f5d8932d9dff073
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-08-07 07:39:33 +0300

    👁️ Eye prefab, script and object on scene

---
+++ Assets/Scripts/Controllers/Objects/Eye.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Controllers.Creatures;
+using UnityEngine;
+using Util.Data;
+
+namespace Controllers.Objects {
+    public class Eye : MonoBehaviour {
+        private Transform Pupil => transform.Find("Pupil");
+
+        [SerializeField] private MinMaxFloat xRange;
+
+        private void Update() {
+            var angle = Vector3.SignedAngle(
+                transform.forward,
+                Player.Instance.transform.position - transform.position,
+                transform.up
+            );
+            Debug.Log(angle);
+            Pupil.transform.localPosition =
+                new Vector2(
+                    Mathf.Pow(Mathf.Abs(angle) / 90, 2) * (angle > 0 ? xRange.Max : xRange.Min),
+                    0
+                );
+        }
+    }
+}
