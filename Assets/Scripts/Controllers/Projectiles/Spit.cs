93e8511e04ad3aa4d781eadff48a84d8fc5955df
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-08-08 13:15:33 +0300

    ⚡ Advanced Spit time slowing split into categories

--- Assets/Scripts/Controllers/Projectiles/Spit.cs
+++ Assets/Scripts/Controllers/Projectiles/Spit.cs
@@ -4,6 +4,8 @@ using Controllers.Creatures.Enemies.Base;
 using Controllers.Projectiles.Base;
 using Core;
 using UnityEngine;
+using Util.Classes;
+using Util.ExtensionMethods;
 
 namespace Controllers.Projectiles {
     public class Spit : Projectile {
@@ -13,12 +15,32 @@ namespace Controllers.Projectiles {
         private float InvasionRadius =>
             invasionRadius * (1.25F * (maxTimeAlive - _timeAliveLeft) + 1F * _timeAliveLeft) / maxTimeAlive;
 
-        [SerializeField] private float slowTimeScale;
-        [SerializeField] private float slowmoTimeout;
-        [SerializeField] private float invasionRadius;
+        [Tooltip("Start slowmo scale coefficient (1 = normal, < 1 = slowed).")] [SerializeField]
+        private float startSlowmoScale;
+
+        [Tooltip("How long time will be slowed after spit launch.")] [SerializeField]
+        private float startSlowmoTimeout;
+
+        [Tooltip("Before invasion (when enemy is near) slowmo scale coefficient (1 = normal, < 1 = slowed).")]
+        [SerializeField]
+        private float beforeInvasionSlowmoScale;
+
+        [Tooltip("How long time will be slowed when spit is near to enemy.")] [SerializeField]
+        private float beforeInvasionSlowmoTimeout;
+
+        [Tooltip("After invasion slowmo scale coefficient (1 = normal, < 1 = slowed).")] [SerializeField]
+        private float afterInvasionSlowmoScale;
+
+        [Tooltip("How long time will be slowed after invasion.")] [SerializeField]
+        private float afterInvasionSlowmoTimeout;
+
+        [Tooltip("Radius of invasion to body.")] [SerializeField]
+        private float invasionRadius;
 
         private float _timeAliveLeft, _circleDefaultLocalScale;
         private bool _killPlayer = true;
+
+        private GameThread _timeManagementThread;
 
         protected override void Awake() {
             _timeAliveLeft = maxTimeAlive;
@@ -26,15 +48,18 @@ namespace Controllers.Projectiles {
 
             CameraScript.Instance.SetTarget(transform, 50, 0.5F);
             GameManager.Instance.SetTargetForAllEnemies(transform);
-
-            Time.timeScale = 1F;
         }
 
         protected override void Start() {
             _circleDefaultLocalScale = Circle.transform.localScale.x;
+
+            Time.timeScale = startSlowmoScale;
+            _timeManagementThread = GameThread.Create().Subscribe(
+                0.1F,
+                () => Time.timeScale = Mathf.Min(Time.timeScale + 0.1F / startSlowmoTimeout, 1));
         }
 
-        protected override void Update() { 
+        protected override void Update() {
             _timeAliveLeft -= Time.deltaTime;
             GetComponent<Light>().range = 200 * Mathf.Sqrt(_timeAliveLeft / maxTimeAlive);
             Halo.GetComponent<Light>().range = 10 * Mathf.Sqrt(_timeAliveLeft / maxTimeAlive);
@@ -58,6 +83,13 @@ namespace Controllers.Projectiles {
                     ? Color.green
                     : Color.white;
 
+            if (list.IsNotEmpty() && maxTimeAlive - _timeAliveLeft > 0.2F) {
+                Time.timeScale = beforeInvasionSlowmoScale;
+                _timeManagementThread.Unsubscribe().Subscribe(
+                    0.1F,
+                    () => Time.timeScale = Mathf.Min(Time.timeScale + 0.1F / beforeInvasionSlowmoTimeout, 1));
+            }
+
             if (Input.GetKeyDown(KeyCode.Mouse1)) {
                 if (list.Count > 0 &&
                     Vector3.Distance(list[0].transform.position, transform.position) <= InvasionRadius) {
@@ -72,16 +104,10 @@ namespace Controllers.Projectiles {
                         );
                     }
 
-                    GlobalScope.ExecuteWithDelay(
-                        slowmoTimeout,
-                        () =>
-                            GlobalScope.ExecuteEveryInterval(
-                                0.1F,
-                                () => Time.timeScale = Mathf.Min(1, Time.timeScale + 0.1F),
-                                () => Time.timeScale >= 1
-                            ),
-                        () => Time.timeScale = slowTimeScale
-                    );
+                    Time.timeScale = afterInvasionSlowmoScale;
+                    _timeManagementThread.Unsubscribe().Subscribe(
+                        0.1F,
+                        () => Time.timeScale = Mathf.Min(Time.timeScale + 0.1F / afterInvasionSlowmoTimeout, 1));
                     GameManager.Instance.RemoveEnemy(list[0]);
                     Destroy(list[0].gameObject);
                 }
